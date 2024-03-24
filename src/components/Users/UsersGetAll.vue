b998e8c24434df1859039e4339fb73da8f2766bb
cbatuic <cbalaman@uic.edu.ph>
2024-03-24 11:15:30 +0800

    Setup Users Routes

---
+++ src/components/Users/UsersGetAll.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Users Get All</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Users/UsersPost.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Users Post</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
